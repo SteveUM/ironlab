e9abfed5da0fba45bf162747d7d949833a775c2a
uncle-meat <3000.sjg+github@gmail.com>
2016-09-30 07:09:04 -0400

    added numpy installer code

--- ILabPythonLib/ironplot/__init__.py
+++ ILabPythonLib/ironplot/__init__.py
@@ -5,6 +5,57 @@ newPath = __path__[0] + "\\bin"
 import sys
 sys.path.append(newPath)
 
+#--- Numpy install
+
+import os
+import sys
+import tempfile
+import zipfile
+from os.path import dirname, isdir, join
+
+def unzip(zip_file, dir_path):
+    """Unzip the zip_file into the directory dir_path."""
+    z = zipfile.ZipFile(zip_file)
+    for name in z.namelist():
+        if name.endswith('/'):
+            continue
+        path = join(dir_path, *name.split('/'))
+        if not isdir(dirname(path)):
+            os.makedirs(dirname(path))
+        fo = open(path, 'wb')
+        fo.write(z.read(name))
+        fo.close()
+    z.close()
+
+def self_install():
+    tmp_dir = tempfile.mkdtemp()
+    egg_path = 'numpy-2.0.0-1.egg'
+    egg_path2 = 'scipy-1.0.0-2.egg'
+    unzip(egg_path, tmp_dir)
+    unzip(egg_path2, tmp_dir)
+    sys.path.insert(0, tmp_dir)
+    import egginst
+    print "Bootstrapping:", egg_path
+    ei = egginst.EggInst(egg_path)
+    ei.install()
+    print "Bootstrapping:", egg_path2
+    ei2 = egginst.EggInst(egg_path2)
+    ei2.install()
+
+#--- /Numpy install
+
+try:
+    import numpy
+except Exception as ex:
+    try:
+        print 'attempting to install numpy for Iron Python...'
+        sys.prefix = os.getcwd()  #base dir of app
+        self_install()
+        x = raw_input('IronLab must be restarted')
+    except Exception as ex:
+        print 'problem while intalling numpy for IP: ', str(ex)
+
+
 import ironplot_windows
 import ironplot_functions
 import ironplot_mscharts
