2913823b2f84780d4e79c70b4235874b3d60c3aa
uncle-meat <3000.sjg+github@gmail.com>
2016-09-30 07:03:41 -0400

    Created named application

---
+++ IronLabApp/App.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using IronPythonConsole;
+
+namespace IronLabApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            PythonConsoleWindow w = new PythonConsoleWindow();
+            w.ConsoleInitialized += WOnConsoleInitialized;
+            MainWindow = w;
+            w.Show();
+        }
+
+        private void WOnConsoleInitialized(object sender, EventArgs eventArgs)
+        {
+            //PythonConsoleWindow pcw = (PythonConsoleWindow) sender;
+            //pcw.PythonScope.SetVariable("Window", pcw);
+            //ScriptSource  script = pcw.PythonScope.Engine.CreateScriptSourceFromString("print 2*3", SourceCodeKind.Statements);
+            //script.Execute();
+        }
+    }
+}
